2ad983b193c5ad9c04e69e5afa6895a715a6922f
AntonFdiaz <anton.fdiaz@icloud.com>
2025-04-12 21:56:08 +0200

    main stuff

---
+++ main.py
@@ -0,0 +1,38 @@
+from PySide6.QtWidgets import *
+from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtGui import *
+from PySide6.QtCore import *
+import src.toolbar as toolbar
+from urllib.parse import urlparse
+import sys
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("SearcherWeb")
+        self.setGeometry(100,100,1050,700)
+        icon = QIcon()
+        icon.addFile("images/icon.png")
+        self.setWindowIcon(icon)
+        self.webview = QWebEngineView()
+        self.webview.setUrl("https://www.google.com")
+        self.webview.urlChanged.connect(self.url_changed)
+        layout = QVBoxLayout()
+        layout.setContentsMargins(0,0,0,0)
+        layout.addWidget(self.webview)
+        widget = QWidget()
+        widget.setLayout(layout)
+        toolbar.add_toolbar(self)
+        self.setCentralWidget(widget)
+    def url_changed(self,url):
+        self.old_url = url
+        domain = urlparse(url.toString()).netloc
+        if domain:
+            self.setWindowTitle(f"{domain} - SearcherWeb")
+        else:
+            self.setWindowTitle("SearcherWeb")
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setStyle("Fusion")
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
